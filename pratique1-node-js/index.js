c8a8a1269bdb7b0d6538d485c143c71158a2cf28
Bastien <bastfv@gmail.com>
2023-06-14 14:59:42 +0200

    message selon durée du contrat

--- pratique1-node-js/index.js
+++ pratique1-node-js/index.js
@@ -1,24 +1,40 @@
 const fs = require('fs/promises');
 
-async function write(firstname, lastname, resiliationService, date) {
+async function write(firstname, lastname, resiliationService, date, duration) {
+
+    console.log("Veuillez patienter.");
+
   try {
     const content = `Madame, Monsieur,
+
     Par la présente, je vous demande de bien vouloir mettre fin au contrat de ${resiliationService}, dès le
-    ${date}.
-    La possibilité de résilier ce contrat m’est offerte puisque le présent contrat est sans engagement.
-    J’ai bien noté, qu‘en application des dispositions contractuelles qui nous lient, cette résiliation devrait
+    ${date}.` 
+    
+    + 
+
+    `${duration <1 ? "La possibilité de résilier ce contrat m’est offerte puisque le présent contrat est sans engagement." : 
+    "La possibilité de résilier ce contrat m’est offerte car la durée d'engagement de " + `${duration}` + " an est dépassée. "}` 
+    
+    +
+     
+    `J’ai bien noté, qu‘en application des dispositions contractuelles qui nous lient, cette résiliation devrait
     prendre effet immédiatement ou selon les délais prévus dans le cas d’une résiliation anticipée.
+
     Bien cordialement,
     ${firstname} ${lastname}`;
-    await fs.writeFile('./courrier-resiliation.txt', content);
+
+    await fs.writeFile(`./courrier-${resiliationService}.txt`, content);
+    console.log("Le fichier a été créé avec succès.");
   } catch (err) {
     console.log(err);
-  }
-}
+  };
+};
 
 const arguments = process.argv;
 const lastname = arguments[2];
 const firstname = arguments[3];
 const resiliationService = arguments[4];
 const date = arguments[5];
-write(firstname, lastname, resiliationService, date);
+const duration = arguments[6];
+
+write(firstname, lastname, resiliationService, date, duration);
